111adb01b51db73e276e86295ecc0b09db0d0d59
Judielle Villa <judiellezinke@yahoo.de>
2022-06-11 13:29:46 +0200

    ContactForm edited

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
   };
 
   return (
-    <div className="d-flex justify-content-between align-items-center py-2 px-5">
+    <div className="d-flex justify-content-between align-items-center py-2 px-5" style={{backgroundColor: 'transparent'}}>
       <div className="logo">
         <img
           style={logoStyle}
