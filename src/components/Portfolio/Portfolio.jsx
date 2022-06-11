765c23725110ba8dced7a03bac5e146d688d7aac
Judielle Villa <judiellezinke@yahoo.de>
2022-06-11 21:50:33 +0200

    added animation and transitions to portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,37 +5,109 @@ export default function Portfolio() {
   return (
     <div>
       <div className="container">
-        <div className="fade-overlay">
         <div className="gallery-picture-tl gallery-picture">
-            <div className="gallery-caption">
-                <h2>Placeholder</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, dolore!</p>
-            </div>
+          <div className="gallery-caption">
+            <h2>Placeholder</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+              dolore!
+            </p>
+          </div>
+          <div className="gallery-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
+              consequuntur esse deserunt exercitationem inventore distinctio
+              modi eaque. Odio fuga incidunt ex, commodi vitae exercitationem
+              laudantium, ducimus similique nostrum sequi sed doloremque?
+              Repellat ab earum exercitationem? Temporibus consectetur quam,
+              totam, quia ullam obcaecati eius nihil nemo odio cum quos
+              aspernatur necessitatibus.
+            </p>
+          </div>
         </div>
-        </div>
+
         <div className="gallery-picture-bl gallery-picture">
-        <div className="gallery-caption">
-                <h2>Placeholder</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, dolore!</p>
-            </div>
+          <div className="gallery-caption">
+            <h2>Placeholder</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+              dolore!
+            </p>
+          </div>
+          <div className="gallery-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
+              consequuntur esse deserunt exercitationem inventore distinctio
+              modi eaque. Odio fuga incidunt ex, commodi vitae exercitationem
+              laudantium, ducimus similique nostrum sequi sed doloremque?
+              Repellat ab earum exercitationem? Temporibus consectetur quam,
+              totam, quia ullam obcaecati eius nihil nemo odio cum quos
+              aspernatur necessitatibus.
+            </p>
+          </div>
         </div>
+
         <div className="gallery-picture-center gallery-picture">
-        <div className="gallery-caption">
-                <h2>Placeholder</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, dolore!</p>
-            </div>
+          <div className="gallery-caption">
+            <h2>Placeholder</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+              dolore!
+            </p>
+          </div>
+          <div className="gallery-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
+              consequuntur esse deserunt exercitationem inventore distinctio
+              modi eaque. Odio fuga incidunt ex, commodi vitae exercitationem
+              laudantium, ducimus similique nostrum sequi sed doloremque?
+              Repellat ab earum exercitationem? Temporibus consectetur quam,
+              totam, quia ullam obcaecati eius nihil nemo odio cum quos
+              aspernatur necessitatibus.
+            </p>
+          </div>
         </div>
+
         <div className="gallery-picture-tr gallery-picture">
-        <div className="gallery-caption">
-                <h2>Placeholder</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, dolore!</p>
-            </div>
+          <div className="gallery-caption">
+            <h2>Placeholder</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+              dolore!
+            </p>
+          </div>
+          <div className="gallery-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
+              consequuntur esse deserunt exercitationem inventore distinctio
+              modi eaque. Odio fuga incidunt ex, commodi vitae exercitationem
+              laudantium, ducimus similique nostrum sequi sed doloremque?
+              Repellat ab earum exercitationem? Temporibus consectetur quam,
+              totam, quia ullam obcaecati eius nihil nemo odio cum quos
+              aspernatur necessitatibus.
+            </p>
+          </div>
         </div>
+
         <div className="gallery-picture-br gallery-picture">
-        <div className="gallery-caption">
-                <h2>Placeholder</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, dolore!</p>
-            </div>
+          <div className="gallery-caption">
+            <h2>Placeholder</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+              dolore!
+            </p>
+          </div>
+          <div className="gallery-text">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
+              consequuntur esse deserunt exercitationem inventore distinctio
+              modi eaque. Odio fuga incidunt ex, commodi vitae exercitationem
+              laudantium, ducimus similique nostrum sequi sed doloremque?
+              Repellat ab earum exercitationem? Temporibus consectetur quam,
+              totam, quia ullam obcaecati eius nihil nemo odio cum quos
+              aspernatur necessitatibus.
+            </p>
+          </div>
         </div>
       </div>
     </div>
