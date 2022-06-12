1dd5863bf04a661032392ac4c09b8db2d90a3e77
Judielle Villa <judiellezinke@yahoo.de>
2022-06-13 00:58:59 +0200

    fixed aboutus page positioning

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -51,7 +51,7 @@ export default function Navbar({
 
 
   return (
-    <div className={`${location.pathname=== "/" && "fixed-top"}`}  style={{backgroundColor: scrollNavColor}}>
+    <div className={`${location.pathname=== "/" && "fixed-top"} ${location.pathname=== "/about-us" && "fixed-top"}`}  style={{backgroundColor: scrollNavColor}}>
       <div
         className="d-flex justify-content-between align-items-center py-2 px-5 "
         style={{  color: textColor }}
