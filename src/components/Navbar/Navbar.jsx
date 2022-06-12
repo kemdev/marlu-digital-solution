631ba960bb2e8747e93c9c8ef84972b523c921a4
Judielle Villa <judiellezinke@yahoo.de>
2022-06-13 01:51:13 +0200

    fixed several things and make scrollbar thiccer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -48,7 +48,7 @@ export default function Navbar({
   return (
 
 
-    <div className={`${fixed}`} style={{ backgroundColor: scrollNavColor, transition: 'all 0.2s ease-in' }}>
+    <div className={`${fixed} ${location.pathname==='/about-us' && 'fixed-top'}`} style={{ backgroundColor: scrollNavColor, transition: 'all 0.2s ease-in' }}>
 
       <div
         className="d-flex justify-content-between align-items-center py-2 px-5 "
